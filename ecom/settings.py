fed1698ad70cb5af5faea4322a5ba2ca469de25f
moonn15 <moon.shrestha15@gmail.com>
2022-06-13 16:16:26 +0545

    API

--- ecom/settings.py
+++ ecom/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static/"]
+import os
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- home/models.py
+++ home/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
 # Create your models here.
+# category
+# subcategory
+# ads
+# slider
+# product
+# brand
+
+class Category(models.Model):
+	name = models.CharField(max_length = 300)
+	slug = models.TextField(unique = True)
+	status = models.CharField(max_length = 50,blank = True,choices = (('active','Active'),('','Default')))
+
+
+	def __str__(self):
+		return self.name
+
+
+class SubCategory(models.Model):
+	name = models.CharField(max_length = 300)
+	category = models.ForeignKey(Category,on_delete = models.CASCADE,default =1)
+	slug = models.TextField(unique = True)
+
+	def __str__(self):
+		return self.name
+
+class Ad(models.Model):
+	name = models.CharField(max_length = 300)
+	image = models.ImageField(upload_to = 'media')
+	link = models.URLField(max_length = 500,blank = True)
+	rank = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
+class Slider(models.Model):
+	name = models.CharField(max_length = 400)
+	image = models.ImageField(upload_to = 'media')
+	rank = models.IntegerField()
+	status = models.CharField(max_length = 50,blank = True,choices = (('active','Active'),('','Default')))
+
+	def __str__(self):
+		return self.name
+
+
+LABELS = (('offer','offer'),('new','new'),('hot','hot'),('','default'))
+class Product(models.Model):
+	name = models.CharField(max_length = 500)
+	image = models.ImageField(upload_to = 'media')
+	price = models.IntegerField()
+	discounted_price = models.IntegerField(default = 0)
+	category = models.ForeignKey(Category,on_delete = models.CASCADE)
+	subcategory = models.ForeignKey(SubCategory,on_delete = models.CASCADE)
+	description = models.TextField(blank = True)
+	status = models.CharField(max_length = 50,choices = (('active','Active'),('','Default')))
+	labels = models.CharField(max_length = 500,choices = LABELS,blank = True)
+	slug = models.TextField(unique = True)
+
+
+	def __str__(self):
+		return self.name
+
+
+class Cart(models.Model):
+	username = models.CharField(max_length = 400)
+	slug = models.CharField(max_length= 500)
+	items = models.ForeignKey(Product,on_delete = models.CASCADE)
+	quantity = models.IntegerField(default = 1)
+	checkout = models.BooleanField(default = False)
+	total = models.IntegerField(default = 1)
+	
+	def __str__(self):
+		return self.username
